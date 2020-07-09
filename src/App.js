c9b9e09a3e3c5dd36fd6523ff0d3040924de1f22
Dairogil23 <dairo.gil@landsoft.com.co>
2020-07-09 07:31:54 -0500

    React-router-dom and Bootstrap added in the project.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
+    <BrowserRouter>
+      <Switch>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
